6d258f80e3b5e7c5703c2224c07e9d7e4d499c44
Mitality <96748385+Mitality@users.noreply.github.com>
2024-09-26 22:39:52 +0200

    + Option to round health placeholder to an int

--- src/main/java/bodyhealth/depend/PlaceholderAPI.java
+++ src/main/java/bodyhealth/depend/PlaceholderAPI.java
@@ -31,22 +31,31 @@ public class PlaceholderAPI extends PlaceholderExpansion {
 
         if (params.startsWith("health_")) {
             String[] splitParams = params.split("_");
-            if (splitParams.length > 1) {
 
-                String bodyPartName = splitParams.length == 2 ? splitParams[1].toUpperCase() : splitParams[1].toUpperCase() + "_" + splitParams[2].toUpperCase();
+            if (splitParams.length >= 2) {
+                StringBuilder bodyPartNameBuilder = new StringBuilder();
 
+                for (int i = 1; i < splitParams.length; i++) {
+                    if (splitParams[i].equalsIgnoreCase("rounded")) {
+                        break;
+                    }
+                    if (i > 1) bodyPartNameBuilder.append("_");
+                    bodyPartNameBuilder.append(splitParams[i].toUpperCase());
+                }
+
+                String bodyPartName = bodyPartNameBuilder.toString();
                 BodyPart bodyPart;
                 try {
                     bodyPart = BodyPart.valueOf(bodyPartName);
                 } catch (IllegalArgumentException e) {
                     return "Invalid body part: " + bodyPartName;
                 }
-
                 if (player.isOnline()) {
                     Player onlinePlayer = player.getPlayer();
                     if (onlinePlayer != null) {
                         BodyHealth bodyHealth = BodyHealthUtils.getBodyHealth(onlinePlayer);
                         double partHealth = bodyHealth.getHealth(bodyPart);
+                        if (splitParams[splitParams.length - 1].equalsIgnoreCase("rounded")) return String.valueOf((int) Math.round(partHealth));
                         return String.format("%.2f", partHealth); // Return health as a formatted string
                     }
                 }
