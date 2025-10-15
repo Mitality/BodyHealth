fd1aea0a0163096e09616e8d63ef0a440f5bf304
Mitality <96748385+Mitality@users.noreply.github.com>
2025-10-15 02:46:48 +0200

    Fix kill-with-command not working on Folia

--- src/main/java/bodyhealth/effects/effect/KILL_PLAYER.java
+++ src/main/java/bodyhealth/effects/effect/KILL_PLAYER.java
@@ -1,12 +1,12 @@
 package bodyhealth.effects.effect;
 
+import bodyhealth.Main;
 import bodyhealth.config.Config;
 import bodyhealth.config.Debug;
 import bodyhealth.core.BodyPart;
 import bodyhealth.effects.BodyHealthEffect;
 import bodyhealth.effects.EffectType;
 import org.bukkit.Bukkit;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class KILL_PLAYER implements BodyHealthEffect {
@@ -30,8 +30,12 @@ public class KILL_PLAYER implements BodyHealthEffect {
     public void onApply(Player player, BodyPart part, String[] args, boolean isRecovery) {
 
         if (Config.kill_with_command) {
-            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-            Bukkit.dispatchCommand(console, "kill " + player.getName());
+            if (!Main.getScheduler().isGlobalThread()) {
+                Main.getScheduler().execute(() ->
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kill " + player.getName()));
+            } else {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kill " + player.getName());
+            }
         } else {
             if (player.getHealth() > 0) player.setHealth(0);
         }
